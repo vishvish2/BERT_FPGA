87e1371ad3f03f9ec4ee9ed1bb0831afe1517a18
vishvish2 <vishnubarath909@gmail.com>
2026-01-26 18:23:41 +0000

    Attempted to display error count as binary value on LEDs on breadboard (unsuccessful)

--- Rx/counter.sv
+++ Rx/counter.sv
@@ -1,19 +1,19 @@
 module counter #(
-    parameter int MAX_COUNT = 100
+    parameter int MAX_COUNT = 100000
 ) (
     input  logic        clk,
     input  logic        rstn,   // Active-low synchronous reset
-    output logic [6:0]  out,
+    output logic [16:0]  out,
     output logic        error
 );
 
   always_ff @(posedge clk) begin
     if (!rstn) begin
-      out   <= '0;
+      out   <= 17'b0;
       error <= 1'b0;
     end
     else if (out == MAX_COUNT-1) begin
-      out   <= '0;
+      out   <= 17'b0;
       error <= 1'b1;   // one-cycle pulse
     end
     else begin
--- Rx/reset_counter.sv
+++ Rx/reset_counter.sv
@@ -1,16 +1,16 @@
 module reset_counter (
     input  clk,               // Clock
     input  rstn,              // Active-low synchronous reset
-    output reg [7:0] out,      // 7-bit counter output
+    output reg [22:0] out,      // Counter output
 	 output reset
 );
 
   always @(posedge clk) begin
     if (!rstn) begin
-      out <= 8'd0;
+      out <= 23'd0;
 		reset <= 0;
-    end else if (out == 8'd200) begin
-      out <= 8'd0;
+    end else if (out == 23'd5000000) begin
+      out <= 23'd0;
 		reset <= 0;
     end else begin
       out <= out + 1'b1;
